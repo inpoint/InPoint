e4462403b015626d7019df74bf0cc8eea3d26f05
Ville Miekk-oja <ville.miekk-oja@aalto.fi>
2013-03-23 13:30:58 +0200

    positions and zoom in & out

--- gen/com/example/inpoint/R.java
+++ gen/com/example/inpoint/R.java
@@ -15,14 +15,17 @@ public final class R {
         public static final int df_map_v2=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int map_mark=0x7f020003;
+        public static final int zoom_in=0x7f020004;
+        public static final int zoom_out=0x7f020005;
     }
     public static final class id {
-        public static final int HorizontalScrollView1=0x7f070001;
-        public static final int ScrollView01=0x7f070003;
-        public static final int imageView3=0x7f070002;
-        public static final int menu_settings=0x7f070005;
-        public static final int textStatus=0x7f070004;
+        public static final int ScrollView01=0x7f070004;
+        public static final int imageView3=0x7f070001;
+        public static final int menu_settings=0x7f070006;
+        public static final int textStatus=0x7f070005;
         public static final int textView1=0x7f070000;
+        public static final int zoomInButton=0x7f070002;
+        public static final int zoomOutButton=0x7f070003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/example/inpoint/InPoint.java
+++ src/com/example/inpoint/InPoint.java
@@ -11,6 +11,8 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
@@ -20,12 +22,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.StrictMode;
 import android.util.DisplayMetrics;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -50,7 +56,12 @@ public class InPoint extends Activity {
 	public static Handler mainHandler;
 	public static TextView textStatus;
 	public static ImageView imageView;
-
+	
+	private static ImageButton zoomInButton;
+	private static ImageButton zoomOutButton;
+	private DisplayMetrics dm;
+	
+	
 	public static boolean should_scan;
 
 	private Bitmap mapMark;
@@ -70,10 +81,14 @@ public class InPoint extends Activity {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
 
 		// Setup UI
 		textStatus = (TextView) findViewById(R.id.textStatus);
 		imageView = (ImageView) this.findViewById(R.id.imageView3);
+		zoomInButton = (ImageButton)findViewById(R.id.zoomInButton);
+        zoomOutButton = (ImageButton)findViewById(R.id.zoomOutButton);  
 
 		// Setup WiFi
 		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
@@ -228,8 +243,8 @@ public class InPoint extends Activity {
 								 * for function Test, In actual use, please
 								 * replace "TestString" below to "json"
 								 */
-								String TestString = "35.3,12.2";
-								// readPositionFromServer(TestString);
+								String TestString = "10_0,BrainStorm";
+							    //readPositionFromServer(TestString);
 								readPositionFromServer(json);
 
 								// send msg to main thread to update UI
@@ -265,15 +280,22 @@ public class InPoint extends Activity {
 						mapCopy = BitmapFactory.decodeResource(getResources(),
 								R.drawable.df_map_v2);
 						// add the mark coordinates here, (x,y)
-						mapTemp = addMark(mapCopy, mapMark, serverReturn_x
+		// Version-1: Here using the calculated relative-coordinates, used to displace actual position	
+		/*				mapTemp = addMark(mapCopy, mapMark, serverReturn_x
 								/ mapActualX_Max, serverReturn_y
 								/ mapActualY_Max);
-
+		*/
+						
+		// Version-2: Here using the preset absolute coordinates, used to displace room-level-accuracy position	
+						mapTemp = addMark_roomLevel(mapCopy, mapMark, (int)serverReturn_x);
 						imageView.setImageBitmap(mapTemp);
+						//modify the picture display according to the User Finger Gesture
+						new ImageViewHelper(dm, imageView, mapTemp, zoomInButton, zoomOutButton); 
 					}
 				}
 			};
 		}
+//		new ImageViewHelper(dm, imageView, mapTemp, zoomInButton, zoomOutButton);
 		Log.d(TAG, "onCreate()");
 	}
 
@@ -340,6 +362,9 @@ public class InPoint extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	
 
 	public void readPositionFromServer(String src) {
 		String temp[] = src.split(",|\\_|\\ ");
@@ -372,6 +397,7 @@ public class InPoint extends Activity {
 
 	}
 
+// Version-1: Here using the calculated relative-coordinates, used to displace actual position		
 	public Bitmap addMark(Bitmap src, Bitmap mark, float relativePosX,
 			float relativePosY) {
 		// create a new figure, same size as original figure src
@@ -390,13 +416,114 @@ public class InPoint extends Activity {
 		// canvas.drawBitmap(mark, (src.getWidth() - mark.getWidth()) / 2,
 		// (src.getHeight() - mark.getHeight()) / 2, null);
 
+	
 		int posx = (int) (relativePosX * src.getWidth());
-		int posy = (int) (relativePosY * src.getHeight());
+	    int posy = (int) (relativePosY * src.getHeight());	
+		
 		// textStatus.append("\nmapped_x: ");
 		// textStatus.append(Integer.toString(posx));
 		// textStatus.append("  _y: ");
 		// textStatus.append(Integer.toString(posy));
 		canvas.drawBitmap(mark, posx, posy, null);
+		canvas.save(Canvas.ALL_SAVE_FLAG);
+		canvas.restore();
+
+		mark.recycle();
+		mark = null;
+
+		return newb;
+	}
+	
+// Version-2: Here using the preset absolued coordinates, used to displace room-level-accuracy position	
+	public Bitmap addMark_roomLevel(Bitmap src, Bitmap mark, int roomNum) {
+		// create a new figure, same size as original figure src
+		float posx = 0; 
+		float posy = 0;
+		Bitmap newb = Bitmap.createBitmap(src.getWidth(), src.getHeight(),
+				Config.ARGB_8888);//
+		 textStatus.append("map_Width: ");
+		 textStatus.append(Integer.toString(src.getWidth()));
+		 textStatus.append("  _Height: ");
+		 textStatus.append(Integer.toString(src.getHeight()));
+
+		Canvas canvas = new Canvas(newb);
+		canvas.drawBitmap(src, 0, 0, null);// insert original figure at
+											// coordinate-(0,0)
+
+		// insert mark onto the figure
+		// canvas.drawBitmap(mark, (src.getWidth() - mark.getWidth()) / 2,
+		// (src.getHeight() - mark.getHeight()) / 2, null);
+
+		switch (roomNum){
+			case 1:
+				posx = 190;
+				posy = 343;
+				break;
+			case 2:
+				posx = 190;
+				posy = 513;
+				break;
+			case 3:
+				posx = 380;
+				posy = 377;
+				break;
+			case 4:
+				posx = 570;
+				posy = 390;
+				break;
+			case 5:
+				posx = 483;
+				posy = 595;
+				break;
+			case 6:
+				posx = 775;
+				posy = 583;
+				break;
+			case 7:
+				posx = 1083;
+				posy = 571;
+				break;
+			case 8:
+				posx = 1283;
+				posy = 565;
+				break;
+			case 9:
+				posx = 907;
+				posy = 429;
+				break;
+			case 10:
+				posx = 1280;
+				posy = 350;
+				break;				
+			case 11:
+				posx = 1370;
+				posy = 350;
+				break;
+			case 12:
+				posx = 1523;
+				posy = 523;
+				break;
+			case 13:
+				posx = 1523;
+				posy = 369;
+				break;
+			case 14:
+				posx = 1830;
+				posy = 535;
+				break;	
+			default:
+				posx = 15;
+				posy = 15;
+		}
+		
+		 int posx_rev = (int)((posx-15)*1.333);
+		 int posy_rev = (int)((posy-15)*1.333);
+		 textStatus.append("\nmapped_x: ");
+		 textStatus.append(Integer.toString(posx_rev));
+		 textStatus.append("  _y: ");
+		 textStatus.append(Integer.toString(posy_rev));
+		 textStatus.append("\n");
+		canvas.drawBitmap(mark, posx_rev, posy_rev, null);
 		canvas.save(Canvas.ALL_SAVE_FLAG);
 		canvas.restore();
 
